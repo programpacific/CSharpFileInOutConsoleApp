62f0a18e87a5edd8010879d4b9050802c37ef613
programpacific <programpacific@users.noreply.github.com>
2018-08-04 12:55:07 -0600

    Added exercise parameters & changed wording slightly.

--- CSharpFileInOutConsoleApp/Program.cs
+++ CSharpFileInOutConsoleApp/Program.cs
@@ -6,19 +6,25 @@ using System.Threading.Tasks;
 using System.IO;
 
 namespace CSharpFileInOutConsoleApp
+
+   //    IN A CONSOLE APP, CREATE CODE THAT DOES THE FOLLOWING:
+  //1. Ask a user for a number.
+ //2. Log that number to a text file.
+//3. Print the text file back to the user.
+
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the File I/O Demonstration Application!\n\n" +
-                "Though you might not see it here, what I'll do is collect a number, write it to a file,\n" +
+                "Though you might not see it here, what I'll do is collect an input, write it to a file,\n" +
                 "and then write it back to the console by reading the just created file!\n\n" +
-                "I've selected a generic location, C:/User/Users/New/TestLog.txt, although this location may not exist\n" +
-                "on your machine!\n\n");
+                "I've selected a generic location, C:/Users/New/TestLog.txt, although this location may not exist\n" +
+                "on your machine & cause an error!\n\n");
 
             writeMore:
-            Console.Write("What would number would you like to store?: ");
+            Console.Write("What would you like to store?: ");
             string TestLog = Console.ReadLine().ToLower(); ;
 
             using (StreamWriter file = new StreamWriter(@"C:\Users\New\Desktop\TestLog.txt", true))
@@ -29,9 +35,9 @@ namespace CSharpFileInOutConsoleApp
             string StoredValue = File.ReadAllText(@"C:\Users\New\Desktop\TestLog.txt");
 
             // You could at this point convert it into an "int", but not much reason to do it at this point of the application, unless it's built upon at a later point.
+            // The way it is designed now allows for a little bit more usability and was good practice!
 
-            //string StoredInt = StoredValue;
-            //int FinalIntValue = Int32.Parse(StoredInt);
+            //int IntValue = Int32.Parse(StoredInt); // You'd change then below code to write this value instead.
 
             // But here it is anyways! Although if you were to handle more than one number as the program is currently designed you'd have to implement some special handling 
             //of the text files writing format. With only one integer it works perfectly, only when two values have been entered will it cause an error.
